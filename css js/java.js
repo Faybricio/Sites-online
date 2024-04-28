bddd76f8fdbbb8bcb7670158c34e26779f9a6860
Faybricio <166452609+Faybricio@users.noreply.github.com>
2024-04-28 16:30:23 -0400

    Atualizar o java.js
    Corrigindo erro no tema

--- css js/java.js
+++ css js/java.js
@@ -1,5 +1,3 @@
-const chk = document.getElementById('chk')
-
-chk.addEventListener('change', () => {
-  document.body.classList.toggle('dark')
-})
+function chek(){
+     document.body.classList.toggle('dark')
+}
